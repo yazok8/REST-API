ab2a072ff2d1b2ce53593e91942b1ef5f96ca380
yazan <yazankhirfan@gmail.com>
2021-10-14 14:07:42 -0400

    add express, nodemon, mongoose to the project

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './db.js'
+
+dotenv.config()
+
+connectDB
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT} `)
+})
